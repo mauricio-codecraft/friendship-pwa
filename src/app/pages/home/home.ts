d087c1ac8ccefb32d4cbf98f1221615411797884
mauricio <mauricio@codecraftit.com>
2020-08-22 10:30:39 -0300

    Pagination support

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -9,10 +9,32 @@ import { API } from 'aws-amplify';
 })
 export class HomePage implements OnInit {
   preorders: any;
+  pagination: URLSearchParams;
+  showLoadMore: boolean;
   constructor(private router: Router) {}
   
   async ngOnInit() {
-    this.preorders = await API.get('friendship', '/preorder/list', null);
-    //console.log('this.preorders = ', this.preorders);
+    this.showLoadMore = true;
+    this.preorders = [];
+    this.loadPreorders();
+  }
+
+  async loadPreorders() {
+    if (this.pagination) {
+      this.pagination.set('page', (+this.pagination.get('page') + 1).toString()); // next page  
+    } else {
+      this.pagination = new URLSearchParams({
+        page: '0',
+        size: '2',
+        sort: 'created_at',
+        order: 'desc'
+      });
+    }
+    let morePreorders = await API.get('friendship', '/preorder/list?' + this.pagination.toString(), null);
+    if (morePreorders && morePreorders.length > 0) {
+      this.preorders = this.preorders.concat(morePreorders);
+    } else {
+      this.showLoadMore = false;
+    }
   }
 }
